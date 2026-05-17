48421d89509d9b2b3480140cafd793d7fdf95826
Wessel Kooy <wesselkooy@gmail.com>
2026-05-17 12:04:17 +0200

    Set up NestJS API shell

---
+++ apps/api/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { appConfig } from "./config/app.config";
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(appConfig().port);
+}
+
+void bootstrap();
